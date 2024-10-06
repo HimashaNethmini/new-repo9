5aab34fb54f945117774fc4a33178f073f64a2d6
HimashaNethmini <himashanethmini05@gmail.com>
2024-10-06 14:36:32 +0530

    make the article list

--- my-blog/src/pages/ArticlesList.js
+++ my-blog/src/pages/ArticlesList.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import articles from "./article-content";
 
 const ArticlesList = () => {
   return (
-    <div>ArticlesList</div>
-  )
-}
+    <>
+      <h1>ALL Articles </h1>
+      {articles.map((article) => (
+        <Link key ={article.name}
+          className="article-list-item" 
+          to={`/articles/${article.name}`}>
+          <h3>{article.title}</h3>
+          <p>{article.content[0].substring(0, 150)}....</p>
+        </Link>
+      ))}
+    </>
+  );
+};
 
-export default ArticlesList
+export default ArticlesList;
