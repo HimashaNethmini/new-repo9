3753a8f28510c15465603827d2ef4f0eb43b0cdf
HimashaNethmini <himashanethmini05@gmail.com>
2024-10-06 14:36:19 +0530

    show articles dynmically using Id

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useParams } from 'react-router-dom';
+import articles from './article-content';
 
 const ArticlePage = () => {
-  return (
-    <div>ArticlePage</div>
-  )
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId);
+
+    return (
+        <>
+        <h1>{article.title}</h1>
+        {article.content.map((paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+        ))}
+        </>
+    );
 }
 
-export default ArticlePage
+export default ArticlePage;
