800494bd7e0b359ecb7a5d410b57a14516cfea5c
HimashaNethmini <himashanethmini05@gmail.com>
2024-10-06 13:24:46 +0530

    import navbar

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -4,19 +4,20 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArtciclesListPage from "./pages/ArticlesList";
 import ArticlePage from "./pages/ArticlePage";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h1> Himasha's Blog </h1>
+        <Navbar />
         <div id="page-body">
           {/* defining routes */}
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/articles" element={<ArtciclesListPage />} />
-            <Route path ="/articles/:articleId" element = { <ArticlePage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
           </Routes>
         </div>
       </div>
