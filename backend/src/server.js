f7f3dbc23654df353686729b1c4ca4f38cb07fd4
HimashaNethmini <himashanethmini05@gmail.com>
2024-10-09 22:40:22 +0530

    server up n running

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,13 @@
-import express from express;
+import express from 'express';
 
 //creating a new express app for the project
 const app = express();
+
+app.get('/hello', (req,res) =>{
+    res.send('Himasha, so pretty');
+});
+
+//make the express listen
+const PORT = app.listen(8000, () => {
+    console.log('Server is listening on', PORT)
+})
