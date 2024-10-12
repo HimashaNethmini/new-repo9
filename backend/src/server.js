f77e534008532ce5691f7bac1df3e10afe5f5922
HimashaNethmini <himashanethmini05@gmail.com>
2024-10-12 10:12:51 +0530

    import connectToDb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,19 @@
 import express from 'express'
-import { MongoClient } from 'mongodb';
+import { db, connectToDb } from db.js;
 
 //fake db for now
 let articlesInfo = [{
     name: 'learn-react',
-    upvotes:0,
-    comment:[]
+    upvotes: 0,
+    comment: []
 }, {
     name: 'learn-node',
-    upvotes:0,
-    comment:[]
+    upvotes: 0,
+    comment: []
 }, {
     name: 'mongodb',
-    upvotes:0,
-    comment:[]
+    upvotes: 0,
+    comment: []
 }]
 
 //creating a new express app for the project
@@ -21,13 +21,8 @@ const app = express();
 app.use(express.json());
 
 //get method
-app.get('/api/articles/:name', async (req,res) => {
+app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
-
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
 
     const article = await db.collection('articles').findOne({ name });
 
@@ -37,29 +32,24 @@ app.get('/api/articles/:name', async (req,res) => {
     } else {
         res.sendStatus(404).send('Article not found !')
     }
-    
+
 
     res.json(article);
 })
 
 //create an upvote endpoint with mongodb
-app.put('/api/articles/:name/upvote', async (req,res) => {
+app.put('/api/articles/:name/upvote', async (req, res) => {
 
     //what artciles need to be upvote
     const { name } = req.params;
 
     //find the artcile with that name
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
-    await client.connect();
-
-    const db = client.db('react-blog-db');
     await db.collection('articles').updateOne({ name }, {
         $inc: { upvotes: 1 },
     })
     const article = await db.collection('articles').findOne({ name });
 
-    if(article) {
-        article.upvotes += 1;
+    if (article) {
         res.send(`The ${name} article now has ${article.upvotes} upvotes`);
     } else {
         res.send('That article doesn\'t exist');
@@ -67,22 +57,28 @@ app.put('/api/articles/:name/upvote', async (req,res) => {
 
 });
 
-//making commenting possible
-app.post('/api/articles/:name/comments', (req,res)=> {
+//making commenting possible with mongodb
+app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
-    const { postedBy, text }=req.body;
+    const { postedBy, text } = req.body;
 
-    const article = articlesInfo.find(a=> a.name === name );
+    //find the article with that name in db.js
+    //make a query to db
+    await db.collection('articles').updateOne({ name }, {
+        $push: { comments: { postedBy, text } },
+    });
+    const article = db.collection('articles').findOne({ name });
 
     if (article) {
-        article.comment.push({ postedBy, text })
         res.send(article.comment);
     } else {
         res.send('That article doesn\'t exist')
     }
 })
 
-//make the express listen
-const PORT = app.listen(8000, () => {
-    console.log('Server is listening on', PORT)
+//make the express listen using connectToDb class
+connectToDb(() => {
+    const PORT = app.listen(8000, () => {
+        console.log('Server is listening on', PORT)
+    })
 })
