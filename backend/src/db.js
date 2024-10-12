c35072c98650e0145e068295036e8f0fd320660d
HimashaNethmini <himashanethmini05@gmail.com>
2024-10-12 10:11:49 +0530

    make a reusable component

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,7 +4,6 @@ let db;
 
 // create a new function
 async function connectToDb(cb) {
-  //find the article with that name
   const client = new MongoClient("mongodb://127.0.0.1:27017");
   await client.connect();
 
