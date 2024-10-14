fd8358c1741a4c3b8ee8dc8e9b7cfe5dbdb464ae
HimashaNethmini <himashanethmini05@gmail.com>
2024-10-14 09:48:29 +0530

    make pages for authentication and login

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -6,6 +6,8 @@ import ArtciclesListPage from "./pages/ArticlesList";
 import ArticlePage from "./pages/ArticlePage";
 import Navbar from "./Navbar";
 import NotFoundPage from "./pages/NotFoundPage"
+import LoginPage from "./pages/LoginPage";
+import CreateAccountPage from "./pages/CreateAccountPage";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
             <Route path="/about" element={<AboutPage />} />
             <Route path="/articles" element={<ArtciclesListPage />} />
             <Route path="/articles/:articleId" element={<ArticlePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/create-account" element={<CreateAccountPage />} />
             <Route path = "*" element={<NotFoundPage />} />
           </Routes>
         </div>
--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
