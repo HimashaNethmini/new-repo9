78a1cb729f3877492a6f6e7605c2f083966b5279
HimashaNethmini <himashanethmini05@gmail.com>
2024-10-14 09:52:59 +0530

    making state variable

--- my-blog/src/pages/LoginPage.js
+++ my-blog/src/pages/LoginPage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const LoginPage = () => {
+    const [email, setEmail ] = useState('');
+    const [password, SetPassword] = useState('')
+    const [ error, SetError ] = useState('')    //to show if wrong pw or user
   return (
     <h1>Login Page</h1>
   )
