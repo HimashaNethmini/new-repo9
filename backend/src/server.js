1375d02385cf4ff2c4e8cc3fcd3b2ef0d6f35b7a
HimashaNethmini <himashanethmini05@gmail.com>
2024-10-16 21:27:42 +0530

    protecting upvote and comment endpoints

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,14 +28,33 @@ let articlesInfo = [{
 const app = express();
 app.use(express.json());
 
+//authenticating
+app.use(async (req, res, next) => {
+    const { authtoken } = req.headers;
+
+    if (authtoken) {
+        try {
+            req.user = await admin.auth().verifyIdToken(authtoken);
+        } catch (error) {
+            res.sendStatus(400);
+        }
+    }
+
+    next();
+})
+
 //get method
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
     //if article exist send it, if doesn't exist send 404
+    //enable one time upvoting by Id
     if (article) {
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.include(uid);
         res.json(article)
     } else {
         res.sendStatus(404).send('Article not found !')
@@ -45,44 +64,58 @@ app.get('/api/articles/:name', async (req, res) => {
     res.json(article);
 })
 
+app.use ((req,res,next) => {
+    if(req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+})
+
 //create an upvote endpoint with mongodb
 app.put('/api/articles/:name/upvote', async (req, res) => {
 
     //what artciles need to be upvote
     const { name } = req.params;
+    const { uid } = req.user;
 
-    //find the artcile with that name
-    await db.collection('articles').updateOne({ name }, {
-        $inc: { upvotes: 1 },
-    })
     const article = await db.collection('articles').findOne({ name });
-
+    
     if (article) {
-        res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+        const upvoteIds = article.upvoteIds || [];
+        const canUpvote = uid && !upvoteIds.include(uid)
+
+        if (canUpvote) {
+            await db.collection('articles').updateOne({ name }, {
+                $inc: { upvotes: 1 },
+                $push: { upvoteIds: uid}
+        });
+    }
+
+        const updatedArticle = await db.collection('articles').findOne({ name });
+        res.json(updatedArticle)
     } else {
         res.send('That article doesn\'t exist');
     }
-
 });
 
 //making commenting possible with mongodb
 app.post('/api/articles/:name/comments', async (req, res) => {
     const { name } = req.params;
-    const { postedBy, text } = req.body;
+    const { text } = req.body;
+    const { email } = req.user;
 
-    //find the article with that name in db.js
-    //make a query to db
     await db.collection('articles').updateOne({ name }, {
-        $push: { comments: { postedBy, text } },
+        $push: { comments: { postedBy: email, text } },
     });
-    const article = db.collection('articles').findOne({ name });
+    const article = await db.collection('articles').findOne({ name });
 
     if (article) {
-        res.send(article.comment);
+        res.json(article);
     } else {
-        res.send('That article doesn\'t exist')
+        res.send('That article doesn\'t exist!');
     }
-})
+});
 
 //make the express listen using connectToDb class
 connectToDb(() => {
